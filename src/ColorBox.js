0984374e5cd1bfdfeaf6532a0430626001fcdd6c
JohnZBurnett <tuninator@gmail.com>
2018-07-14 14:58:51 -0400

    refactor w/ inline conditional:

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,16 +3,10 @@ import React, { Component } from 'react';
 export default class ColorBox extends Component {
 
   render() {
-      if (this.props.opacity >= 0.200000) {
-        console.log(this.props.opacity)
-        debugger;
         return(
           <div className="color-box" style={{opacity: this.props.opacity}}>
-            <ColorBox opacity={this.props.opacity - 0.1} />
+            {this.props.opacity >= 0.2 ? <ColorBox opacity={this.props.opacity - 0.1}/> : null}
           </div>
         )
-      } else {
-        return(null)
-        }
       }
   }
